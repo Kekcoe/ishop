d86bf9024b7add25878821e029f521483fff6d7a
kekcoe <kekcoe@gmail.com>
2023-05-25 22:48:07 +0300

    Передача данных между компонентами типа SENDREDIRECT с помощью параметров

--- src/main/java/net/devstudy/servlet/HelloWorld.java
+++ src/main/java/net/devstudy/servlet/HelloWorld.java
@@ -13,9 +13,9 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException, ServletException {
-//        String attr1 = (String) req.getAttribute("attr1");
-        String attr1 = (String)req.getSession().getAttribute("attr1");
-        req.getSession().removeAttribute("attr1");
+        String attr1 = req.getParameter("attr1");
+       // String attr1 = (String)req.getSession().getAttribute("attr1");
+//        req.getSession().removeAttribute("attr1");
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         out.println("Hello world: "+attr1);
