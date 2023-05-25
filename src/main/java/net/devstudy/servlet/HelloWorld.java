89d3f9f7a80a6d4b81c7dd63952a41c35151ec30
kekcoe <kekcoe@gmail.com>
2023-05-25 22:28:06 +0300

    Передача данных между компонентами типа FORWARD

--- src/main/java/net/devstudy/servlet/HelloWorld.java
+++ src/main/java/net/devstudy/servlet/HelloWorld.java
@@ -13,9 +13,10 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException, ServletException {
+        String attr1 = (String) req.getAttribute("attr1");
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("Hello world");
+        out.println("Hello world: "+attr1);
 //out.close();
     }
 }
