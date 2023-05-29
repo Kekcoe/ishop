18401207d22eb4ff2b04b1d7dc7fe7858069a1dc
razinoe <kekcoe@gmail.com>
2023-05-29 12:14:56 +0300

    Тема Cookies

--- src/main/java/net/devstudy/servlet/HelloWorldServlet.java
+++ src/main/java/net/devstudy/servlet/HelloWorldServlet.java
@@ -2,6 +2,7 @@ package net.devstudy.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,17 +13,20 @@ import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
-    public void init() throws ServletException {
-        super.init();
-    }
-
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         out.println("<html><body>");
         out.println("Hello world!");
         out.println("</body></html>");
+
+
+        Cookie c = new Cookie("author", "kekcoe");
+        c.setMaxAge(1800); // if negative,the cookie is not stored; if zero, deletes the cookie
+
+        c.setPath("/");
+        c.setHttpOnly(true);
+        resp.addCookie(c);
         out.close();
     }
 }
